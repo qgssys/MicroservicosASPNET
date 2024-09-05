8b79f43646d3bf41bf6cf72c87f6b6dea48b4a53
Welinton <welintonbp@gmail.com>
2024-09-04 21:15:20 -0300

    Criando DbContext MySQL - ConnectioString no program

--- GeekShooping/GeekShopping.ProductAPI/Program.cs
+++ GeekShooping/GeekShopping.ProductAPI/Program.cs
@@ -1,3 +1,6 @@
+using GeekShopping.ProductAPI.Model.Context;
+using Microsoft.EntityFrameworkCore;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
@@ -6,6 +9,19 @@ builder.Services.AddControllers();
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
+
+//aqui eu busco a connectionString da model Base e Context criada.
+var connection = builder.Configuration["MySQLConnection:MySQLConnectionString"];
+builder.Services.AddDbContext<MySQLContext>(options => options.UseMySql(connection, 
+                                                new MySqlServerVersion(
+                                                    new Version(8, 4, 0)
+                                                    )
+                                                )
+                                            );
+//Fim serviço do dbContext/options
+
+
+
 
 var app = builder.Build();
 
