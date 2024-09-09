ccdbc1edee241763c55e4de3b643cff0b786e560
Welinton <welintonbp@gmail.com>
2024-09-09 09:12:46 -0300

    PRIMEIRO CONTROLLER e IMAGES

--- GeekShooping/GeekShopping.ProductAPI/Model/Context/MySQLContext.cs
+++ GeekShooping/GeekShopping.ProductAPI/Model/Context/MySQLContext.cs
@@ -8,5 +8,12 @@ namespace GeekShopping.ProductAPI.Model.Context
 
         public MySQLContext(DbContextOptions<MySQLContext> options) : base(options){}
 
+        // DbSet adiciona a tabela ao contexto
+        public DbSet<Product> Products { get; set;}
+
+        ///Para adicionar no banco de dados execute o migrations no package manager console executando o comando:
+        ///add-migration AddProductDataTableOnDB  (comando mais nome da migration)
+
+
     }
 }
--- GeekShooping/GeekShopping.ProductAPI/Program.cs
+++ GeekShooping/GeekShopping.ProductAPI/Program.cs
@@ -1,14 +1,12 @@
+using AutoMapper;
+using GeekShopping.ProductAPI.Config;
 using GeekShopping.ProductAPI.Model.Context;
+using GeekShopping.ProductAPI.Repository;
 using Microsoft.EntityFrameworkCore;
 
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
-
-builder.Services.AddControllers();
-// Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
-builder.Services.AddEndpointsApiExplorer();
-builder.Services.AddSwaggerGen();
 
 //aqui eu busco a connectionString da model Base e Context criada.
 var connection = builder.Configuration["MySQLConnection:MySQLConnectionString"];
@@ -20,7 +18,21 @@ builder.Services.AddDbContext<MySQLContext>(options => options.UseMySql(connecti
                                             );
 //Fim serviço do dbContext/options
 
+//AUTOMAPPER
+//Adicionando o registro da productVO MappingConfig - AutoMapper
+IMapper mapper = MappingConfig.RegisterMaps().CreateMapper();
+builder.Services.AddSingleton(mapper);
+//builder.Services.AddAutoMapper(AppDomain.CurrentDomain.GetAssemblies());
 
+//Aqui adiciono o scopo  do IProductRepository/ProductRepository implementado
+builder.Services.AddScoped<IProductRepository, ProductRepository>();
+//AUTOMAPPER-FIM
+
+
+builder.Services.AddControllers();
+// Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
+builder.Services.AddEndpointsApiExplorer();
+builder.Services.AddSwaggerGen();
 
 
 var app = builder.Build();
